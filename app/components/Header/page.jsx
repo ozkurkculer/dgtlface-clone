d1d3a719893d4a992ddbad5e7a177551566912af
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-07 22:40:29 +0300

    fix: change header transparency

--- app/components/Header/page.jsx
+++ app/components/Header/page.jsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import Navbar from '../Navbar/page';
 import { FaTelegram, FaPhone } from "react-icons/fa6";
 
+
 export default function Header(){
     return(
-        <div className="sticky z-20 top-0 px-10 py-5 flex flex-row items-center justify-between bg-white bg-blend-color-burn">
+        <div className="sticky z-20 top-0 px-10 py-5 flex flex-row items-center justify-between bg-white/80 bg-blend-saturation ">
             <Image src={'https://dgtlface.com/wp-content/uploads/2022/08/DGTLFACE_400X400_Siyah-300x300-1-e1663175143749.png'} width={225} height={225} alt='Digitalface Logo' />
             <Navbar />
             <div className="flex flex-row gap-5">
