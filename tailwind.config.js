06d2e9d64cefdefd4c49b461599d1a0c529b50ad
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-07 22:39:44 +0300

    style: add background hover effect classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,19 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    mode: 'jit',
     extend: {
       colors: {
         primary: '#46c6c5',
         secondary: '#3b3663',
-      }
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
     },
   },
   plugins: [],
